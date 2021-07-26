4d67fcb544919ea4a8884b9777c954d17726362d
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-07-26 15:55:38 -0400

    Improved

--- api/stripe/index.php
+++ api/stripe/index.php
@@ -7,6 +7,7 @@ class StripeWrapper {
 	public function createCardToken() {
 		$stripe = new \Stripe\StripeClient('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 		
+
 		$tokenObj = $stripe->tokens->create([
 			'card' => [
 				'number' => '4242424242424242',
@@ -15,7 +16,23 @@ class StripeWrapper {
 				'cvc' => '314',
 			],
 		]);
+		
 
 		return $tokenObj;
+	}
+
+	public function createOneDollarCharge($token) {
+		$stripe = new \Stripe\StripeClient('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+
+		$chargeObj = $stripe->charges->create([
+			'amount' => 100,
+			'currency' => 'usd',
+			'source' => $token,
+			'description' => '',
+		]);
+
+
+		return $chargeObj;
 	}
 }
