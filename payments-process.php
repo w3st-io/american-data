5da3a0b9e2f2729f1af8f000613ca5eeeaa4796c
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-07-26 04:59:30 -0400

    s

--- payments-process.php
+++ payments-process.php
@@ -2,11 +2,14 @@
 // [REQUIRE] Personal //
 require_once('./api/billsby/index.php');
 
+
 // [INCLUDE] //
 include('./connection.php');
 
 
+// [BILLSBY]
 $BillsBy = new BillsBy();
+
 
 // [INIT] //
 $payment_method_token = $_POST['payment_method_token'];
