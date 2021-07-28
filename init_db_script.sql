8399a8060b0d75dc9181754fa5589d4a73475acd
aleem-ahmed <opall.io2020@gmail.com>
2021-07-28 14:38:43 -0400

    Improved

--- init_db_script.sql
+++ init_db_script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 27, 2021 at 09:45 PM
+-- Generation Time: Jul 28, 2021 at 08:38 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -24,6 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `password_v_codes`
+--
+
+CREATE TABLE `password_v_codes` (
+  `email` varchar(255) NOT NULL,
+  `v_code` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `email` varchar(255) NOT NULL,
+  `stripe_pi_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub`
+--
+
+CREATE TABLE `sub` (
+  `email` varchar(255) NOT NULL,
+  `stripe_sub_id` int(255) NOT NULL,
+  `current_period_end` int(20) NOT NULL,
+  `current_period_reports_count` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,7 +68,7 @@ CREATE TABLE `users` (
   `phone` int(12) NOT NULL,
   `sign` varchar(10000) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `stripe_customer_token` varchar(255) DEFAULT NULL,
+  `stripe_cus_id` varchar(255) DEFAULT NULL,
   `payment_jwt` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
