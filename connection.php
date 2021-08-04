81bb2c1e08b37a63e8592479aecf973a7eaf0ff9
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-04 02:32:35 -0400

    s

--- connection.php
+++ connection.php
@@ -12,6 +12,4 @@ $conn = new mysqli(DB_HOST, DB_USER_, DB_PASSWORD_, DB_NAME_);
 
 
 // Check connection
-if ($conn->connect_error) {
-	die('Connection failed: ' . $conn->connect_error);
-}
+if ($conn->connect_error) { die('Connection failed: ' . $conn->connect_error); }
