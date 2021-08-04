4fd64ed40b0760f6a251b0bfa295978caae6e674
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-03 23:52:22 -0400

    Improved

---
+++ config/index-template.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'americandata');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('TITLE', 'american-data');
+define('STRIPE_KEY', '');
+define('SECRET_JWT_KEY', '');
