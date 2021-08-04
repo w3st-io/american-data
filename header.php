ca1361e23f756ebc6db7631a76a0c474e7d214da
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-04 13:25:23 -0400

    s

--- header.php
+++ header.php
@@ -43,6 +43,9 @@
 	</script>
 	<!-- End Google Tag Manager -->
 
+	<!-- Microsoft tag -->
+	<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"56383622"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
+
 	<!-- Template CSS -->
 </head>
 
