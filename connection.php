b8b63382ed6a999d5bef9bb0fe4f20ee9ef283fe
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-04 01:53:36 -0400

    s

--- connection.php
+++ connection.php
@@ -10,6 +10,7 @@ $conn = '';
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER_, DB_PASSWORD_, DB_NAME_);
 
+
 // Check connection
 if ($conn->connect_error) {
 	die('Connection failed: ' . $conn->connect_error);
