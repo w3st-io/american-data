9ba1ae45463419b11bfa392ab3f5c3dbdfb0d55d
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-05 00:41:10 -0400

    i

--- config/index.php
+++ config/index.php
@@ -2,6 +2,7 @@
 // [INIT] //
 define('TITLE', 'americandata');
 define('DB_HOST', 'localhost');
+define('DATABASE_NAME', 'vin_vehicles');
 define('VINDECODER_API_KEY', 'c404ea350amsh3a1bf345dd7386fp1bcde5jsnad8d954aa8d4');
 define('SECRET_JWT_KEY', 'a54b94bc3b94d6a330a859f37b9231e571a0f7966d2c44557e219ad7440c80ef4d2');
 
@@ -14,14 +15,17 @@ else { define('PRODUCTION', 'false'); }
 
 
 // [PRODUCTION-VAR] //
-if (PRODUCTION == 'true') {
-	define('DATABASE_NAME', 'vin_vehicles');
+if (PRODUCTION == 'true' && $_SERVER['SERVER_NAME'] == 'www.americanvinhistory.com') {
 	define('DATABASE_USER', 'vin_vehicles');
 	define('DATABASE_PASSWORD', 'E$&FR-6AJ)xw');
 	define('STRIPE_KEY', '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');
 }
+else if (PRODUCTION == 'true' && $_SERVER['SERVER_NAME'] == 'www.vinvehiclehistoryreports.us.org') {
+	define('DATABASE_USER', 'root');
+	define('DATABASE_PASSWORD', '');
+	define('STRIPE_KEY', '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');
+}
 else {
-	define('DATABASE_NAME', 'americandata');
 	define('DATABASE_USER', 'root');
 	define('DATABASE_PASSWORD', '');
 	define('STRIPE_KEY', 'sk_test_51INvnfCC0rHo3XXZxdgGXsFDstmtEnCGYux6ZA8XlySkrSsYqHAa5kOFptGb8k2w6TtyOAuJhiBpeeTkXShldA6E00XuTKIQ3h');
